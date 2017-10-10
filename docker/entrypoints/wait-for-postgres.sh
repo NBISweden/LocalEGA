137e985deab1153abac145ef13f8e776d3b207c4
Dmytro Titov <dmytro.titov@gmail.com>
2017-10-10 18:28:15 +0200

    Fix variables.

--- docker/entrypoints/wait-for-postgres.sh
+++ docker/entrypoints/wait-for-postgres.sh
@@ -6,11 +6,11 @@ cmd="$@"
 SLEEPTIME=1
 RETRIES=30
 
-export PGPASSWORD=$POSTGRES_PASSWORD
+export PGPASSWORD=${POSTGRES_PASSWORD}
 
-until psql -U $POSTGRES_USER -h ega_db -c "select 1" || [ $RETRIES -eq 0 ]; do
+until psql -U ${POSTGRES_USER} -h ega_db -c "select 1" || [ ${RETRIES} -eq 0 ]; do
     echo "Waiting for Postgres server, $((RETRIES--)) remaining attempts..."
-        sleep SLEEPTIME;
+        sleep ${SLEEPTIME};
     done
 
 >&2 echo "Postgres is up - executing command"
