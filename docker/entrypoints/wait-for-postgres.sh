47b07773fd4fa556ff309c36e6c7dc532e7a3d5f
Dmytro Titov <dmytro.titov@gmail.com>
2017-10-10 18:21:45 +0200

    Add max attempts to wait-for-postgres.sh

--- docker/entrypoints/wait-for-postgres.sh
+++ docker/entrypoints/wait-for-postgres.sh
@@ -3,10 +3,15 @@
 set -e
 
 cmd="$@"
+SLEEPTIME=1
+RETRIES=30
 
 export PGPASSWORD=$POSTGRES_PASSWORD
 
-until psql -U $POSTGRES_USER -h ega_db -c "select 1"; do sleep 1; done
+until psql -U $POSTGRES_USER -h ega_db -c "select 1" || [ $RETRIES -eq 0 ]; do
+    echo "Waiting for Postgres server, $((RETRIES--)) remaining attempts..."
+        sleep SLEEPTIME;
+    done
 
 >&2 echo "Postgres is up - executing command"
 exec $cmd
