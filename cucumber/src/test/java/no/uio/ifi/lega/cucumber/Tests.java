35e6ad4a98c256d11d32985a9a17ec50b9547cfd
Dmytro Titov <dmytro.titov@gmail.com>
2017-10-12 14:36:10 +0200

    Cucumber: add authentication feature and sample scenario.

---
+++ cucumber/src/test/java/no/uio/ifi/lega/cucumber/Tests.java
@@ -0,0 +1,13 @@
+package no.uio.ifi.lega.cucumber;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        format = {"pretty", "html:target/cucumber"},
+        features = "classpath:authentication.feature"
+)
+public class Tests {
+}
