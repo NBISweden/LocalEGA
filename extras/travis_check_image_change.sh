b24d7517954fb04b0667d4060b3cbfbae2040149
Jocke <j.bygdell@gmail.com>
2018-11-27 08:36:46 +0100

    Rebuild base os every 30 days
    The base image that we use as base build for all the other containers should be rebuilt monthly to make sure that it is as up to date as possible.

--- extras/travis_check_image_change.sh
+++ extras/travis_check_image_change.sh
@@ -5,14 +5,16 @@
 # If we detect changes against origin/dev we build and push new image.
 
 check_image () {
-  if git diff --exit-code origin/dev -- "images/$1/Dockerfile" >/dev/null
+  old=$(date +%s --date '30 days ago')
+  dhub=$(date +%s --date=$(curl -X GET https://registry.hub.docker.com/v2/repositories/nbisweden/ega-$1/tags/latest/ | jq -r '.last_updated'))
+  if [ $old \> $dhub ]
   then
-    printf 'New ega-%s docker image is not required.\n' "$1"
-  else
-    printf 'ega-%s base image changed, building new ega-%s image.\n' "$1" "$1"
+    printf 'ega-%s base image is to old, building new ega-%s image.\n' "$1" "$1"
     make -C images "$1"
     printf 'pushing new ega-%s image.\n' "$1"
     docker push "nbisweden/ega-$1"
+  else
+    printf 'New ega-%s docker image is not required.\n' "$1"
   fi
 }
 
